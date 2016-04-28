96080c814073ad96e7d9eb9ee032b8c6e5c52a49
Jacob Patton <jrobertpatton@gmail.com>
2016-04-28 15:27:20 -0500

    ajax added

--- routes/songs.js
+++ routes/songs.js
@@ -1,20 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('You forgot to reqest a new song with node-rest-client');
-});
-
 var Client = require('node-rest-client').Client;
-
 var client = new Client();
 
-client.get("https://api.spotify.com/v1/artists/5a2EaR3hamoenG9rDuVn8j/top-tracks?country=us", function (data, response) {
-  for (var i = 0 ; i < data.tracks.length; i++){
-
-   console.log(data.tracks[i].name);
-  }
+/* GET users listing. */
+router.get('/', function(req, res, next){
+  client.get("https://api.spotify.com/v1/artists/5a2EaR3hamoenG9rDuVn8j/top-tracks?country=us", function (data, response){
+  res.json(data)
+ })
 });
 
 module.exports = router;
