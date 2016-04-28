588161f7d145afb3e1c48fb4a43839609fedc612
Jacob Patton <jrobertpatton@gmail.com>
2016-04-28 14:40:21 -0500

    api working

--- routes/songs.js
+++ routes/songs.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('You forgot to reqest a new song with node-rest-client');
 });
 
+var Client = require('node-rest-client').Client;
+
+var client = new Client();
+
+client.get("https://api.spotify.com/v1/artists/5a2EaR3hamoenG9rDuVn8j/top-tracks?country=us", function (data, response) {
+  for (var i = 0 ; i < data.tracks.length; i++){
+
+   console.log(data.tracks[i].name);
+  }
+});
+
 module.exports = router;
